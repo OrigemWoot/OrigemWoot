385d69ff1f1ce0223d18b718ad47edb0599214ad
LuizEdP <luiz.eduardo@mrpbrasil.com>
2015-11-29 11:42:41 -0200

    Update emotes.js

--- JSON/emotes.js
+++ JSON/emotes.js
@@ -1229,7 +1229,7 @@ window.ow_emotes = {
          "url":"https://i.imgur.com/ypzUakJ.png",
          "width":"71px"
       },
-      "penes2"{
+      "penes2":{
          "height":"225px",
          "url":"https://i.imgur.com/b3Vv0RI.png",
          "width":"105"
