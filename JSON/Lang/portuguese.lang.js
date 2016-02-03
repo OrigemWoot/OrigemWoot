7d4bd8812450d99ac3a147f1afd8d0870d469902
Igorce9 <uchihagammes@gmail.com>
2016-02-03 16:15:57 -0300

    Update portuguese.lang.js

--- JSON/Lang/portuguese.lang.js
+++ JSON/Lang/portuguese.lang.js
@@ -7,7 +7,7 @@ window.ow_lang = {
     "titleHelper":        "Ajudante do Origem Woot",
     "titleSpreader":      "Divulgador do Origem Woot",
 	"titleDesigner":   "Designer do Origem Woot",
-	"owload":	  "Origem Woot v%%Version%% ativado!",
+	"owload":	  "Origem Woot",
 	"version":	  "Versão",
 	"reloading":	  "Recarregando o Origem Woot",
 	"reseting":	  "Resetando o Origem Woot",
