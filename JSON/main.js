11e02aed120d70d8052995f5649847b2394998e1
Ciker <itznaktuminajasiftw@gmail.com>
2017-01-05 13:44:48 -0800

    Update main.js

--- JSON/main.js
+++ JSON/main.js
@@ -92,7 +92,7 @@ window.ow_main = {
 	        "https://i.imgur.com/q8EEUif.jpg",
 	        "https://i.imgur.com/LeXuIc0.jpg"
 	],
-	"socket" : "wss://test-nakituminajasi.c9users.io/socket/websocket",
+	"socket" : "wss://test-nakituminajasi.c9users.io:8080/socket/websocket",
 	"lng": {
 		"en" : {
 			"url" : "https://rawgit.com/CikerDeveloper/origem-repo/master/JSON/Lang/english.lang.js",
