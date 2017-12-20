17fba5218b2b5874f208c83b18688a1e5f01ff0b
Ciker <itznaktuminajasiftw@gmail.com>
2017-12-20 22:22:33 +0100

    Update sklang.js

--- JSON/Lang/sklang.js
+++ JSON/Lang/sklang.js
@@ -4,7 +4,7 @@ window.ow_lang = {
     "titleDev":           "Origem Woot Dev",
     "titlevip":           "Origem Woot Vip",
     "titleumvip":"Ultra Music VIP",
-    "titleHelper":        "Origem Woot Pomocník",
+    "titleStaff":        "Origem Woot Staff",
     "titleSpreader":      "Origem Woot Spreader",
 	"titleDesigner": 	  "Origem Woot Designer",
 	"owload":             "Origem Woot v%%Version%% načtena!",
