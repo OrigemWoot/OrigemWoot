541244aaadade2abc0ebf592426b5ef146b80da9
Ciker <itznaktuminajasiftw@gmail.com>
2018-07-24 14:36:36 +0200

    Update main.js

--- JSON/main.js
+++ JSON/main.js
@@ -92,7 +92,7 @@ window.ow_main = {
 	        "https://i.imgur.com/q8EEUif.jpg",
 	        "https://i.imgur.com/LeXuIc0.jpg"
 	],
-	"socket" : "wss://ws.origem-woot.tk",
+	"socket" : "wss://origemsocket.herokuapp.com/socket/websocket",
 	"lng": {
 		"en" : {
 			"url" : "https://rawgit.com/OrigemWoot/OrigemWoot/master/JSON/Lang/english.lang.js",
